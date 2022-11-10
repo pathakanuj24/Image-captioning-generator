56848e161b5d829bd841549e8d6561c8520d7759
Anuj Pathak <pathakanuj807@gmail.com>
2022-11-10 11:12:43 +0530

    final commit

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div><footer class="text-center text-white" style= {{backgroundColor: '#432C7A'}}  >
+    <div class="container pt-9">
+      <div class="flex justify-center mb-9">
+    
+      </div>
+    </div>
+  
+    <div class="text-center text-white- p-10" style={{backgroundColor: '#432C7A'}}>
+      © 2022 Copyright:- 
+      <a class="text-white-800">Image2text</a>
+    </div>
+  </footer>
+  </div>
+  )
+}
+
+export default Footer
